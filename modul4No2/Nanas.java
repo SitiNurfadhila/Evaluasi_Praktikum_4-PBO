03ec6e9012e56944a12a49379bb3d4aa1cea364b
SitiNurfadhila <127620903+SitiNurfadhila@users.noreply.github.com>
2023-04-28 06:40:28 -0700

    Add files via upload

---
+++ modul4No2/Nanas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul4_no2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Nanas extends Buah{
+    @Override
+    public void bervitamin(){
+        System.out.println("Nanas adalah buah yang bervitamin C");
+    }
+}
